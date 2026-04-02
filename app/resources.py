a9202cc8619dedc75ae8194efeb185a8c1695eb4
swapnildagade1213 <111967597+swapnildagade1213@users.noreply.github.com>
2026-04-02 23:33:05 +0530

    Update resources.py

--- app/resources.py
+++ app/resources.py
@@ -1,4 +1,4 @@
-from fastmcp import Resource
+from fastmcp.resources import Resource
 
 def register_resources():
     return [Resource(uri="resource://hello", mimeType="text/plain", get=lambda: "Hello from FastMCP on PythonAnywhere!")]
