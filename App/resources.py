fc9e9cbf13ef7142657b9359441db6c0f2039869
swapnildagade1213 <111967597+swapnildagade1213@users.noreply.github.com>
2026-04-02 23:26:31 +0530

    Delete App directory

--- App/resources.py
+++
@@ -1,4 +0,0 @@
-from fastmcp import Resource
-
-def register_resources():
-    return [Resource(uri="resource://hello", mimeType="text/plain", get=lambda: "Hello from FastMCP on PythonAnywhere!")]
--- App/tools.py
+++
@@ -1,4 +0,0 @@
-from fastmcp import Tool
-
-def register_tools():
-    return [Tool(name="echo", description="Echoes the input string", input_schema={"type": "object", "properties": {"text": {"type": "string"}}}, func=lambda text: f"Echo: {text}")]
